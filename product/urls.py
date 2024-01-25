b85642c6b8f9e660cd18f558ffed3c91a955e931
baha_2002 <baktiarzaksylykov22@gmail.com>
2024-01-25 16:48:44 +0600

    Product create

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from product.views import ProductListView, product_detail
+from product.views import ProductListView, ProductListRest, ProductDetailView, ProductCreateView
 
 urlpatterns = [
     path('list/', ProductListView.as_view(), name='list'),
-    path('detail/<int:pk>/', product_detail, name='detail')
+    path('detail/<int:pk>/', ProductDetailView.as_view(), name='detail'),
+    path('list_rest/', ProductListRest.as_view(), name='list_rest'),
+    path('create/', ProductCreateView.as_view(), name='create'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from product.models import Product
 from django.views.generic import TemplateView
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from product.serializers import ProductSerializer
+from rest_framework.response import Response
+
+from product.models import Product
 
 
 class ProductListView(TemplateView):
@@ -11,6 +17,44 @@ class ProductListView(TemplateView):
         return {'products': products}
 
 
-def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-    return render(request, 'product_detail.html', {'product': product})
+class ProductDetailView(TemplateView):
+    template_name = 'product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        product = Product.objects.get(pk=kwargs.get('pk'))
+        return {'product': product}
+
+
+class ProductListRest(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProductCreateView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = ProductSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # 1
+        # Product.objects.create(
+        #     title=serializer.validated_data['title'],
+        #     description=serializer.validated_data['description'],
+        #     price=serializer.validated_data['price'],
+        #     size=serializer.validated_data['size'],
+        #     color=serializer.validated_data['color'],
+        #     image=serializer.validated_data['image']
+        # )
+
+        # 2
+        # Product.objects.create(**serializer.validated_data)
+
+        # 3
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
